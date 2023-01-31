f05ef146c1b79475bac8da298bb3f6158cfd2af5
James Courtois <jcourtois@gmail.com>
2023-01-31 11:09:02 -0500

    solution

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,12 @@ struct Solution {}
 #[cfg(test)]
 impl Solution {
     pub fn reverse(x: i32) -> i32 {
-        0
+        let sign = x.signum();
+        let reversed = x.abs().to_string().bytes().rev().collect();
+        match String::from_utf8(reversed).unwrap().parse::<i32>() {
+            Ok(num) => sign * num,
+            Err(_) => 0
+        }
     }
 }
 
@@ -13,7 +18,52 @@ mod tests {
     use super::*;
 
     #[test]
-    fn reversing_zero() {
+    fn _0_is_0() {
         assert_eq!(Solution::reverse(0), 0);
+    }
+
+    #[test]
+    fn _1_is_1() {
+        assert_eq!(Solution::reverse(1), 1);
+    }
+
+    #[test]
+    fn _2_is_2() {
+        assert_eq!(Solution::reverse(2), 2);
+    }
+
+    #[test]
+    fn _neg_1_is_neg_1() {
+        assert_eq!(Solution::reverse(-1), -1);
+    }
+
+    #[test]
+    fn _21_is_21() {
+        assert_eq!(Solution::reverse(12), 21);
+    }
+
+    #[test]
+    fn _123_is_321() {
+        assert_eq!(Solution::reverse(123), 321);
+    }
+
+    #[test]
+    fn _neg_123_is_321() {
+        assert_eq!(Solution::reverse(-123), -321);
+    }
+
+    #[test]
+    fn _120_is_21() {
+        assert_eq!(Solution::reverse(120), 21);
+    }
+
+    #[test]
+    fn _positive_overflow() {
+        assert_eq!(Solution::reverse(2147483647), 0);
+    }
+
+    #[test]
+    fn _negative_overflow() {
+        assert_eq!(Solution::reverse(2147483647), 0);
     }
 }
